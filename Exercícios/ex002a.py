5ebd619b0a927081216907fa6e261e973776120a
J-o-n-a-s <97320039+J-o-n-a-s@users.noreply.github.com>
2023-07-24 19:01:49 -0300

    Adição de mais arquivos de exercícios
    Adição de mais arquivos de exercícios

---
+++ Exercícios/ex002a.py
@@ -0,0 +1,2 @@
+nome = input('Por gentileza, digite o seu nome: ')
+print('Seja bem-vindo,', nome)
---
+++ Exercícios/ex002b.py
@@ -0,0 +1,2 @@
+nome = input('Por gentileza, digite o seu nome: ')
+print(f'Seja bem-vindo, {nome}')
