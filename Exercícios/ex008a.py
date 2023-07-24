1865c1e9ee1d4fdd621a6bfbcb4d2ffe393ca8c9
J-o-n-a-s <97320039+J-o-n-a-s@users.noreply.github.com>
2023-07-24 19:47:51 -0300

    Adicionados mais arquivos de exercícios
    Adicionados mais alguns arquivos de exercícios.

---
+++ Exercícios/ex008a.py
@@ -0,0 +1,2 @@
+medida = float(input('Digite uma medida em metros: '))
+print(f'A medidade {medida}m corresponde a {(medida * 100):.0f}cm e {(medida * 1000):.0f}mm.')
---
+++ Exercícios/ex010a.py
@@ -0,0 +1,2 @@
+cotacao = float(input('Quanto dinheiro você tem agora na carteira? R$ '))
+print(f'Com {cotacao:.2f} reais você consegue comprar {(cotacao / 4.73):.2f} dólares.')
