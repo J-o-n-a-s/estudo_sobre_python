41c7083882a37dc35bea0b5f82982a33e4a77d35
J-o-n-a-s <97320039+J-o-n-a-s@users.noreply.github.com>
2023-07-31 13:27:29 -0300

    Atualizando o arquivo do exercício 039

--- Exercícios/ex039a.py
+++ Exercícios/ex039a.py
@@ -16,8 +16,10 @@ idade = ano_atual - ano_nascimento
 print(f'Quem nasceu em {ano_nascimento} têm {idade} anos em {ano_atual}.\n')
 
 if idade < 18:
-    print(f'Você ainda não precisa se alistar, falta(m) {18 - idade} ano(s)')
+    print(f'Você ainda não precisa se alistar, falta(m) {18 - idade} ano(s).')
+    print(f'Seu alistamento será em {ano_nascimento + 18}.')
 elif idade == 18:
-   print(f'Você completa ou completou {idade} anos e deve se alistar.')
+   print(f'Você completa ou completou {idade} anos este ano e deve se alistar.')
 elif idade > 18:
     print(f'Você já deveria ter se alistado há {idade - 18} ano(s).')
+    print(f'Seu alistamento deveria ter sido no ano de {ano_nascimento + 18}.')
