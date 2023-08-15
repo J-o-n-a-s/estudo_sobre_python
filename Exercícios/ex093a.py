3b90615adbd13dc8d9550fec3285da0facacb299
J-o-n-a-s <97320039+J-o-n-a-s@users.noreply.github.com>
2023-08-14 21:01:53 -0300

    Adicionados arquivos com solução do exercício 093

---
+++ Exercícios/ex093a.py
@@ -0,0 +1,36 @@
+'''
+Crie um programa que gerencie o aproveitamento de um jogador de futebol.
+O programa vai ler o nome do jogador e quantas partidas ele jogou. 
+Depois vai ler a quantidade de gols feitos em cada partida.
+No final, tudo isso será guardado em um dicionário, incluindo o total de gols feitos durante o campeonato.
+'''
+
+aproveitamento = {}
+gols = []
+total = 0
+
+aproveitamento['Jogador'] = input('Digite o nome do jogador: ').strip()
+numero_partidas = int(input(f'Quantas partidas {aproveitamento["Jogador"]} jogou? '))
+
+for contador in range(0, numero_partidas):
+    gols.append(int(input(f'Quantos gols {aproveitamento["Jogador"]} fez na partida {contador + 1}? ')))
+    total += gols[contador]
+    
+aproveitamento['Gols'] = gols
+aproveitamento['Total'] = total
+
+print('-=' * 30 + '-')
+print(aproveitamento)
+print('-=' * 30 + '-')
+
+for chave, valor in aproveitamento.items():
+    print(f'O campo "{chave}" tem o valor {valor}.')
+
+print('-=' * 30 + '-')
+
+print(f'O jogador {aproveitamento["Jogador"]} jogou {len(aproveitamento["Gols"])} partidas.')
+
+for contador in range(0, len(aproveitamento["Gols"])):
+    print(f'  => Na partida {contador + 1}, fez {aproveitamento["Gols"][contador]} gol(s).')
+    
+print(f'Foi um total de {aproveitamento["Total"]} gol(s).')
