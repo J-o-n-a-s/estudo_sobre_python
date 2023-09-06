0844dd5bc201209d740c3c72ebf631192e5f099e
J-o-n-a-s <97320039+J-o-n-a-s@users.noreply.github.com>
2023-09-06 18:29:36 -0300

    Adição do enunciado do exercício

--- Exercícios/ex008a.py
+++ Exercícios/ex008a.py
@@ -1,2 +1,6 @@
+'''
+Crie um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.
+'''
+
 medida = float(input('Digite uma medida em metros: '))
 print(f'A medidade {medida}m corresponde a {(medida * 100):.0f}cm e {(medida * 1000):.0f}mm.')
