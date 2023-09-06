0b1bfcfb91244a18dbbfb7d1da704cdfb18b24ad
J-o-n-a-s <97320039+J-o-n-a-s@users.noreply.github.com>
2023-09-06 18:20:09 -0300

    Adição do enunciado do exercício

--- Exercícios/ex005a.py
+++ Exercícios/ex005a.py
@@ -1,3 +1,7 @@
+'''
+Crie um programa que leia um número inteiro e mostre na tela o seu sucesso e seu antecessor.
+'''
+
 numero = int(input('Digite um número'))
 antecessor = numero - 1
 sucessor = numero + 1
