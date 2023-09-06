11c5b548f5ec2e7875587f62b20fcb675a4b8d38
J-o-n-a-s <97320039+J-o-n-a-s@users.noreply.github.com>
2023-09-06 18:27:42 -0300

    Adição do enunciado do exercício

--- Exercícios/ex007a.py
+++ Exercícios/ex007a.py
@@ -1,3 +1,7 @@
+'''
+Crie um programa que leia duas notas de um aluno, calcule e mostre a sua média.
+'''
+
 nota1 = float(input('Digite a primeira nota do aluno: '))
 nota2 = float(input('Digite a segunda nota do aluno: '))
 print(f'A média entre os valores {nota1:.1f} e {nota2:.1f} é {((nota1 + nota2) / 2):.1f}.')
