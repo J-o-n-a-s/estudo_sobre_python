2bd8c0608c5f20bc5670d0dc6adf948d876854f3
J-o-n-a-s <97320039+J-o-n-a-s@users.noreply.github.com>
2023-09-06 18:14:54 -0300

    Adição do enunciado do exercício

--- Exercícios/ex003a.py
+++ Exercícios/ex003a.py
@@ -1,3 +1,7 @@
+'''
+Crie um programa que leia dois números e mostre o valor da soma entre eles.
+'''
+
 primeiro = int(input('Primeiro número = '))
 segundo = int(input('Segundo número = '))
 print('A soma é', primeiro + segundo)
