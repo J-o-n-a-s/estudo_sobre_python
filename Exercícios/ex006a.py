2cb6e01c928d7b1f3696573adee030baac4db2ca
J-o-n-a-s <97320039+J-o-n-a-s@users.noreply.github.com>
2023-09-06 18:26:19 -0300

    Adição do enunciado do exercício

--- Exercícios/ex006a.py
+++ Exercícios/ex006a.py
@@ -1,3 +1,7 @@
+'''
+Crie um programa que leia um número e mostre o seu dobro, triplo e raiz quadrada.
+'''
+
 numero = int(input('Digite um número'))
 print(f'O dobro de {numero} é {numero * 2}.')
 print(f'O triplo de {numero} é {numero * 3}.')
