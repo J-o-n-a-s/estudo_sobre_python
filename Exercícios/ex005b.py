2da383d2c8fa1084d3330f884c57e724fb6fc6e5
J-o-n-a-s <97320039+J-o-n-a-s@users.noreply.github.com>
2023-09-06 18:20:52 -0300

    Adição do enunciado do exercício

--- Exercícios/ex005b.py
+++ Exercícios/ex005b.py
@@ -1,2 +1,6 @@
+'''
+Crie um programa que leia um número inteiro e mostre na tela o seu sucesso e seu antecessor.
+'''
+
 numero = int(input('Digite um número'))
 print(f'O antecessor de {numero} é {numero - 1} e o sucessor é {numero + 1}.')
