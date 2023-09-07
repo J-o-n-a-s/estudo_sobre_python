6f81190782f54323a4553e4fc656f47285b1b3ca
J-o-n-a-s <97320039+J-o-n-a-s@users.noreply.github.com>
2023-09-07 16:29:26 -0300

    Adição do enunciado do exercício

--- Exercícios/ex013a.py
+++ Exercícios/ex013a.py
@@ -1,2 +1,6 @@
+'''
+Crie um programa que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
+'''
+
 aumento = float(input('Qual é o salário atual do funcionário: R$'))
 print(f'O novo salário do funcionário é R$ {(aumento * 1.15):.2f}')
