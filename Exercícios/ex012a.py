4c3feba1fec5c31f23c4fa33e1b8decf0ff0c5c7
J-o-n-a-s <97320039+J-o-n-a-s@users.noreply.github.com>
2023-09-07 16:28:15 -0300

    Adição do enunciado do exercício

--- Exercícios/ex012a.py
+++ Exercícios/ex012a.py
@@ -1,2 +1,6 @@
+'''
+Crie um programa que leia o preço de um produto e mostre o seu novo preço com 5% de desconto.
+'''
+
 promocao = float(input('Digite o preço do produto para ver seu valor com 5% de desconto: R$'))
 print(f'O valor do produto é R$ {promocao:.2f}. Na promoção o produto sai por R$ {(promocao * 0.95):.2f}.')
