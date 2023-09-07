0456a3ab8b96e77a1604ffcff828888de7f02cbf
J-o-n-a-s <97320039+J-o-n-a-s@users.noreply.github.com>
2023-09-07 16:30:22 -0300

    Adição do enunciado do exercício

--- Exercícios/ex014a.py
+++ Exercícios/ex014a.py
@@ -1,2 +1,6 @@
+'''
+Crie um programa que leia a temperatura em graus Celsius e converta para uma temperatura em Fahrenheit.
+'''
+
 temperatura = float(input('Informe a temperatura em ºC: '))
 print(f'A temperatura de {temperatura} ºC corresponde a {(temperatura * 9 / 5 + 32):.1f} ºF.')
