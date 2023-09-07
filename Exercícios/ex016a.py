cf07c8e48493cabf3468fa61fa93c99b5367a477
J-o-n-a-s <97320039+J-o-n-a-s@users.noreply.github.com>
2023-09-07 16:32:58 -0300

    Adição do enunciado do exercício

--- Exercícios/ex016a.py
+++ Exercícios/ex016a.py
@@ -1,2 +1,6 @@
+'''
+Crie um programa que leia um número real qualquer pelo teclado e mostre na tela a sua porção inteira.
+'''
+
 numero = input('Digite um número: ')
 print(f'O valor digitado foi {numero} e a sua porção inteira é {int(float(numero))}.')
