66f83c0360aad298aa01c745ef1b2592e4e5f801
J-o-n-a-s <97320039+J-o-n-a-s@users.noreply.github.com>
2023-09-07 16:26:44 -0300

    Adição do enunciado do exercício

--- Exercícios/ex011a.py
+++ Exercícios/ex011a.py
@@ -1,3 +1,7 @@
+'''
+Crie um programa que leia a largura e a altura de uma parede em metros, calcule a sua área e a quantidade de tinta necessária para pintá-la, sabendo que cada litro de tinta, pinta uma área de 2m².
+'''
+
 largura = float(input('Digite a largura da parede: '))
 altura = float(input('Digite a altura da parede: '))
 area = largura * altura
