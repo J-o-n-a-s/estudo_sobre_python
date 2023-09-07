dd15d44f0c9b9c9440841e2e241baded43c1d32e
J-o-n-a-s <97320039+J-o-n-a-s@users.noreply.github.com>
2023-09-07 16:25:40 -0300

    Adição do enunciado do exercício

--- Exercícios/ex010a.py
+++ Exercícios/ex010a.py
@@ -1,2 +1,7 @@
+'''
+Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar.
+Considere o valor de um dólar = R$ 4,73 (Data: 24/07/2023)
+'''
+
 cotacao = float(input('Quanto dinheiro você tem agora na carteira? R$ '))
 print(f'Com {cotacao:.2f} reais você consegue comprar {(cotacao / 4.73):.2f} dólares.')
