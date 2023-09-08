a0759df28dc1daa10ba8af5299d8861fb2c0356d
J-o-n-a-s <97320039+J-o-n-a-s@users.noreply.github.com>
2023-09-08 18:54:33 -0300

    Correção do enunciado do exercício 108

--- Exercícios/ex108a/ex108a.py
+++ Exercícios/ex108a/ex108a.py
@@ -1,5 +1,5 @@
 '''
-Adapte o código do desafio 107, criando uma função adicional chamada moeda()
+Adapte o código do exercício 107, criando uma função adicional chamada moeda()
 que consiga mostrar os valores como um valor monetário formatado.
 '''
 
