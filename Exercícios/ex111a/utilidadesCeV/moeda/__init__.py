e2af9e8c1cdd91e03de56672df37ac1c2407f2dc
J-o-n-a-s <97320039+J-o-n-a-s@users.noreply.github.com>
2023-09-08 19:17:14 -0300

    Correção do arquivo __init__.py do pacote moeda

--- Exercícios/ex111a/utilidadesCeV/moeda/__init__.py
+++ Exercícios/ex111a/utilidadesCeV/moeda/__init__.py
@@ -1,4 +1,29 @@
-from utilidadesCeV import dado
+def aumentar(valor: float, porcentagem: float, formatar: bool = False):
+    if formatar:
+        return moeda(valor + valor * (porcentagem / 100))
+
+    return valor + valor * (porcentagem / 100)
+
+
+def diminuir(valor: float, porcentagem: float, formatar: bool = False):
+    if formatar:
+        return moeda(valor - valor * (porcentagem / 100))
+
+    return valor - valor * (porcentagem / 100)
+
+
+def dobro(valor: float, formatar: bool = False):
+    if formatar:
+        return moeda(valor * 2)
+
+    return valor * 2
+
+
+def metade(valor: float, formatar: bool = False):
+    if formatar:
+        return moeda(valor / 2)
+
+    return valor / 2
 
 
 def moeda(valor: float) -> str:
@@ -10,8 +35,8 @@ def resumo(valor: float, porcentagem_aumentar:float, porcentagem_diminuir:float)
     print(f'{"RESUMO DO VALOR":^30}')
     print('-' * 30)
     print(f'Preço analisado:   {moeda(valor)}')
-    print(f'Dobro do preço:    {dado.dobro(valor, True)}')
-    print(f'Metade do preço:   {dado.metade(valor, True)}')
-    print(f'{porcentagem_aumentar}% de aumento:    {dado.aumentar(valor, porcentagem_aumentar, True)}')
-    print(f'{porcentagem_aumentar}% de redução:    {dado.diminuir(valor, porcentagem_diminuir, True)}')
+    print(f'Dobro do preço:    {dobro(valor, True)}')
+    print(f'Metade do preço:   {metade(valor, True)}')
+    print(f'{porcentagem_aumentar}% de aumento:    {aumentar(valor, porcentagem_aumentar, True)}')
+    print(f'{porcentagem_aumentar}% de redução:    {diminuir(valor, porcentagem_diminuir, True)}')
     print('-' * 30)
