b35580b1029365aedf008a894a77aece2bfc1841
J-o-n-a-s <97320039+J-o-n-a-s@users.noreply.github.com>
2023-09-11 12:12:23 -0300

    Adição do enunciado do exercício

--- Exercícios/ex019a.py
+++ Exercícios/ex019a.py
@@ -1,3 +1,8 @@
+'''
+Um professor quer sortear um dos seus quatro alunos para apagar o quadro.
+Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome do escolhido.
+'''
+
 import random
 aluno01 = input('Primeiro aluno: ')
 aluno02 = input('Segundo aluno: ')
