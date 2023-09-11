ac4f99e804f773653a85a2062c3e1feb8df21c69
J-o-n-a-s <97320039+J-o-n-a-s@users.noreply.github.com>
2023-09-11 12:11:24 -0300

    Adição do enunciado do exercício

--- Exercícios/ex018b.py
+++ Exercícios/ex018b.py
@@ -1,3 +1,7 @@
+'''
+Crie um programa que leia um ângulo qualquer e mostre na tela o valor do cosseno, seno e tangente desse ângulo.
+'''
+
 from math import cos, radians, sin, tan
 angulo = float(input('Digite o ângulo que deseja: '))
 print(f'O ângulo de {angulo:.2f} tem o cosseno {cos(radians(angulo)):.2f}.')
