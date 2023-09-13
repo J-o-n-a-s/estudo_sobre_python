a3c830afe0da4b800f565d5676bdd2f88040e847
J-o-n-a-s <97320039+J-o-n-a-s@users.noreply.github.com>
2023-09-13 18:31:56 -0300

    Adição do enunciado do exercício

--- Exercícios/ex022a.py
+++ Exercícios/ex022a.py
@@ -1,3 +1,12 @@
+'''
+Crie um programa que leia o nome completo de uma pessoa e mostre:
+ - O nome com todas as letras maiúsculas;
+ - O nome com todas as letras minúsculas;
+ - Quantas letras têm ao todo;
+ - Quantas letras têm sem considerar espaços;
+ - Quantas letras têm o primeiro nome.
+'''
+
 nome = input('Digite o seu nome completo: ').strip()
 print('Analisando o seu nome...')
 print(f'O nome completo em letra maiúscula é {nome.upper()}.')
