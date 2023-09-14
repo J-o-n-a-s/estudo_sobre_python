dac4aec14ee2513b8343429c2d7c2f0d4ed1386f
J-o-n-a-s <97320039+J-o-n-a-s@users.noreply.github.com>
2023-09-14 14:35:29 -0300

    Adição do enunciado do exercício

--- Exercícios/ex023a.py
+++ Exercícios/ex023a.py
@@ -1,3 +1,12 @@
+'''
+Crie um programa que leia um número entre 0 e 9999 e mostre na tela cada um dos dígitos separadamente.
+ - Exemplo "1234":
+   - Milhar = 1
+   - Centena = 2
+   - Dezena = 3
+   - Unidade = 4
+'''
+
 numero = int(input('Digite um número entre 0 e 9999: '))
 milhar = numero // 1000
 centena = (numero - (milhar * 1000)) // 100
