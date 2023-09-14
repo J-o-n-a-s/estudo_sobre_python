74fa73d8ac8feafee473cb956ea36e9a4643ee1a
J-o-n-a-s <97320039+J-o-n-a-s@users.noreply.github.com>
2023-09-14 14:37:29 -0300

    Adição do enunciado do exercício

--- Exercícios/ex024b.py
+++ Exercícios/ex024b.py
@@ -1,3 +1,7 @@
+'''
+Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com "SANTO".
+'''
+
 nome = input('Digite o nome de uma cidade: ').strip()
 resultado = nome[:5].upper() == 'SANTO'
 print(f'O nome da cidade começa com "SANTO"?\n{resultado}.')
