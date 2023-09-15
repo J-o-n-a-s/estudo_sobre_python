d0de597eb1366cc689c24d6b14fc21aa90dab540
J-o-n-a-s <97320039+J-o-n-a-s@users.noreply.github.com>
2023-09-15 19:26:19 -0300

    Adição do enunciado do exercício

--- Exercícios/ex025a.py
+++ Exercícios/ex025a.py
@@ -1,3 +1,7 @@
+'''
+Crie um programa que leia o nome completo de uma pessoa e informe se tem "SILVA" no nome.
+'''
+
 nome = input('Digite o seu nome completo: ').strip()
 resultado = 'SILVA' in nome.upper()
 print(f'O seu nome contém "SILVA"?\n{resultado}.')
