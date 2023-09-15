197b1e8a84d3b5e8edf0d6d52bc0d1804fa9b166
J-o-n-a-s <97320039+J-o-n-a-s@users.noreply.github.com>
2023-09-15 19:28:28 -0300

    Adição do enunciado do exercício

--- Exercícios/ex026a.py
+++ Exercícios/ex026a.py
@@ -1,3 +1,10 @@
+'''
+Crie um programa que leia uma frase pelo teclado e mostre:
+ - Quantas vezes aparece a letra "A";
+ - Em que posição ela aparece a primeira vez;
+ - Em que posição ela aparece a última vez.
+'''
+
 frase = input('Digite uma frase: ').strip()
 print(f'A letra "A" aparece {frase.upper().count("A")} vezes.')
 print(f'A letra "A" aparece a primeira vez na posição {frase.upper().find("A", 0) + 1}.')
