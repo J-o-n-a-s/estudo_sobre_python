d46f560fcff9929c1857b9c9109796fe11610989
J-o-n-a-s <97320039+J-o-n-a-s@users.noreply.github.com>
2023-09-15 19:31:31 -0300

    Adição do enunciado do exercício

--- Exercícios/ex027a.py
+++ Exercícios/ex027a.py
@@ -1,3 +1,10 @@
+'''
+Crie um programa que leia o nome completo de uma pessoa, mostrando o primeiro e o último nome separadamente.
+ - Exemplo "Ana Maria da Silva":
+   - Primeiro = Ana
+   - Último = Silva
+'''
+
 nome = input('Digite seu nome completo: ').strip()
 nome_completo = nome.split()
 print(f'Seu primeiro nome é {nome_completo[0]} e seu último nome é {nome_completo[len(nome_completo) - 1]}.')
