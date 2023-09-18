bfa09f35c714157d80ac420b5c49848d2368c65b
J-o-n-a-s <97320039+J-o-n-a-s@users.noreply.github.com>
2023-09-18 20:51:28 -0300

    Adição do enunciado do exercício

--- Exercícios/ex028c.py
+++ Exercícios/ex028c.py
@@ -1,3 +1,9 @@
+'''
+Crie um programa que faça o computador "pensar" em um número inteiro entre 0 e 5
+e peça para o usuário tentar descobrir qual foi o número escolhido pelo computador.
+O programa deverá escrever na tela se o usuário venceu ou perdeu.
+'''
+
 from random import randint
 from time import sleep
 numero = randint(0, 5)
