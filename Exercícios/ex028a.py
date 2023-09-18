7e9261738fbab1666719d52d8fb3b19a5d902061
J-o-n-a-s <97320039+J-o-n-a-s@users.noreply.github.com>
2023-09-18 20:50:19 -0300

    Adição do enunciado do exercício

--- Exercícios/ex028a.py
+++ Exercícios/ex028a.py
@@ -1,3 +1,9 @@
+'''
+Crie um programa que faça o computador "pensar" em um número inteiro entre 0 e 5
+e peça para o usuário tentar descobrir qual foi o número escolhido pelo computador.
+O programa deverá escrever na tela se o usuário venceu ou perdeu.
+'''
+
 from random import sample
 numero = sample(range(6), 1)[0]
 print('Vou pensar em um número entre 0 e 5. Tenta adivinhar o número que pensei.')
