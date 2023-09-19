eae018a7e99134d8b6da47221cbf6d134fb7bc44
J-o-n-a-s <97320039+J-o-n-a-s@users.noreply.github.com>
2023-09-19 20:51:27 -0300

    Adição do enunciado do exercício

--- Exercícios/ex030a.py
+++ Exercícios/ex030a.py
@@ -1,3 +1,7 @@
+'''
+Crie um programa que leia um número inteiro e mostre na tela se ele é par ou ímpar.
+'''
+
 numero = int(input('Digite um número: '))
 resultado = numero % 2
 if resultado == 0:
