e572400e173a8625ac50718f7e2c545c95fc52f7
J-o-n-a-s <97320039+J-o-n-a-s@users.noreply.github.com>
2023-09-20 08:29:19 -0300

    Adição do enunciado do exercício

--- Exercícios/ex031a.py
+++ Exercícios/ex031a.py
@@ -1,3 +1,8 @@
+'''
+Crie um programa que pergunte a distância de uma viagem em km. Calcule o preço da passagem,
+cobrando R$ 0,50 por km para viagens de até 200 km e cobre R$ 0,45 para viagens mais longas.
+'''
+
 distancia = float(input('Digite a distância em km do seu destino final: '))
 valor_km_normal = 0.50
 valor_km_desconto = 0.45
