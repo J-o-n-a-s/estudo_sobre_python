26ef103933b308535916b2953418d9aae0eabaf9
J-o-n-a-s <97320039+J-o-n-a-s@users.noreply.github.com>
2023-09-20 08:30:29 -0300

    Adição do enunciado do exercício

--- Exercícios/ex032a.py
+++ Exercícios/ex032a.py
@@ -1,3 +1,7 @@
+'''
+Crie um programa que leia um ano qualquer e mostre se ele é BISSEXTO ou não.
+'''
+
 ano = int(input('Digite um ano: '))
 if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
     print(f'O ano de {ano} é BISSEXTO.')
