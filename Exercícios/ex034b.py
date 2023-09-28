00cdb59b6ff87e8675d689dae73b64fb3f1d6b0c
J-o-n-a-s <97320039+J-o-n-a-s@users.noreply.github.com>
2023-09-27 22:30:58 -0300

    Adição do enunciado do exercício

--- Exercícios/ex034b.py
+++ Exercícios/ex034b.py
@@ -1,3 +1,9 @@
+'''
+Crie um programa que pergunte o salário de um funcionário e calcule o valor do seu aumento.
+Para salários superiores a R$ 1.250,00, calcule um aumento de 10%.
+Para os inferiores ou iguais, o aumento será de 15%.
+'''
+
 salario_base = 1250.00
 salario = float(input('Digite o salário do funcionário: R$ '))
 if salario > salario_base:
